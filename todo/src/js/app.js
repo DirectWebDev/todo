6f5c0d2e6363a3f494375654f9207bfd0d5e6e02
Tom <45734421+DirectWebDev@users.noreply.github.com>
2019-01-02 20:51:19 +0100

    Add files via upload
    test commit

---
+++ todo/src/js/app.js
@@ -0,0 +1,9 @@
+var app = new Vue ({
+    el: "#app",
+    data: {
+        tasks: [
+            {desc: 'Katze fütter', fertig: false},
+            {desc: 'Katze fütter', fertig: false}
+        ]
+    }
+});
